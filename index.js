0a0e65b2d633bdf473e92a266f766313acc04e10
JOSEPH IZUU <eeagleyee@gmail.com>
2023-12-19 11:20:12 +0100

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Middleware to verify working hours
 const workingHoursMiddleware = (req, res, next) => {
@@ -37,7 +38,7 @@ app.get("/contact", workingHoursMiddleware, (req, res) => {
 });
 
 // Start server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
